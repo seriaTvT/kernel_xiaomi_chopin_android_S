aa8134987485551b4d64fd307c4b53231628a75e
seriaTvT <63958697+seriaTvT@users.noreply.github.com>
2026-04-13 20:30:32 +0800

    lmao

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -365,7 +365,7 @@ static int input_get_disposition(struct input_dev *dev,
 	*pval = value;
 	return disposition;
 }
-obj-$(CONFIG_KSU) += kernelsu/#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU
 extern bool ksu_input_hook __read_mostly;
 extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
 #endif
